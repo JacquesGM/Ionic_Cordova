cdd83afc02c7aede27d69b8c196399506521d862
Jacques <jacquesgaldino@gmail.com>
2024-01-03 11:45:44 -0300

    second commit

---
+++ app-Agenda/src/pages/home/home.ts
@@ -0,0 +1,100 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController) {
+
+  }
+
+  dadosAgenda = [
+  {
+  	texto:'Prova de matemática',
+  	data:'2025-02-08',
+  	ativo:true
+  },
+  {
+  	texto:'Treino de futebol',
+  	data:'2025-03-22',
+  	ativo:false
+  }];
+
+  ativarCompromisso(index){
+
+    let menssagem = 'Compromisso "' + this.dadosAgenda[index].texto;
+
+    if (this.dadosAgenda[index].ativo == true) {
+      menssagem =menssagem + ' "ativado';
+    }else{
+      menssagem = menssagem + ' " foi desativado';
+
+      const toast = this.toastCtrl.create({
+        message: menssagem,
+        position: 'middle',
+        duration: 3000
+      });
+
+      toast.present();
+    }
+  }
+
+  mudarStatus(value:boolean){
+    this.dadosAgenda.forEach((item) =>{
+      item.ativo = value;
+    });
+  }
+
+  adicionar(){
+    const alert = this.alertCtrl.create({
+      title: 'Opa!',
+      message: 'Criar novo compromisso?',
+      buttons: [
+      {
+        text:'Criar',
+        handler:(val) => {
+          this.dadosAgenda.push(
+          {
+            texto:'',
+            data: '',
+            ativo: true
+          }
+
+          );
+        }
+      },
+      {
+        text: 'Cancelar'
+      }
+      ]
+    });
+
+    alert.present();
+  }
+
+  limpar(){
+    const alert = this.alertCtrl.create({
+      title: 'Opa!',
+      message: 'Realmente deseja apagar todos os compromissos?',
+      buttons: [
+      {
+        text:'Limpar',
+        handler:(val) => {
+          this.dadosAgenda = [];
+        }
+      },
+      {
+        text:'Cancelar',
+      }
+      ]
+    });
+
+    alert.present();
+  }
+
+  }
